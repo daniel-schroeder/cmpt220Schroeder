3d62131fdfa56a8f5981daf75914845608fc41da
daniel-schroeder <daniel.schroeder1@marist.edu>
2017-01-24 20:21:22 -0500

    Lab1 Code

--- Labs/Lab 1/ConvertToFahrenheit.java
+++ Labs/Lab 1/ConvertToFahrenheit.java
@@ -11,7 +11,7 @@ public class ConvertToFahrenheit {
     Scanner input = new Scanner(System.in); //creates a new scanner object
     System.out.print("Enter a degree in celsius"); //asks the user to input a degree in celsius
     double celsius = input.nextDouble(); //creates a double using user input 
-    double fahrenheit = 9.0 / 5 * celsius + 32; //calculates the temp. in fahrenheit
+    double fahrenheit = (9.0 / 5.0) * celsius + 32; //calculates the temp. in fahrenheit
     System.out.println("The temperature in fahrenheit is" + fahrenheit); //prints out new temp
   }
 }
